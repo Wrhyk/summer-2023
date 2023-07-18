957596b6d44dc2d7025c70397b8ec3119a61b015
william <williamkarlsen1@hotmail.com>
2023-07-18 18:30:15 +0100

    modified existing tables to prevent repetitions, added 2 new tables and inserted data into table fish_types

--- schema.sql
+++ schema.sql
@@ -4,20 +4,93 @@ CREATE TABLE [users] (
     nickname VARCHAR(45) NOT NULL PRIMARY KEY,
     first_name VARCHAR(45) NOT NULL,
     last_name VARCHAR(45) NOT NULL,
-    password CHAR(60) NOT NULL,
-    fish_id INT,
-    FOREIGN KEY ("fish_id") REFERENCES fish (fish_id)
+    password CHAR(60) NOT NULL
 );
 
 
 DROP TABLE IF EXISTS[fish]
 
 CREATE TABLE [fish] (
-    fish_id INT NOT NULL PRIMARY KEY,
+    fish_id INT PRIMARY KEY,
     fish_type VARCHAR(60) NOT NULL,
-    size VARCHAR(20) NOT NULL,
+    f_weight VARCHAR(20) NOT NULL,
+    f_length INT NOT NULL,
     x_location DECIMAL(30) NOT NULL,
     y_location DECIMAL(30) NOT NULL,
     nickname varchar(45) NOT NULL,
-    FOREIGN KEY (nickname) REFERENCES users (nickname)
+    FOREIGN KEY (nickname) REFERENCES users (nickname),
+    FOREIGN KEY (fish_type) REFERENCES fish_types (fish_type)
 );
+
+DROP TABLE IF EXISTS [basket]
+
+CREATE TABLE [basket] (
+    nickname VARCHAR(45) NOT NULL PRIMARY KEY,
+    fish_id INT,
+    FOREIGN KEY (nickname) REFERENCES users (nickname),
+    FOREIGN KEY (fish_id) REFERENCES fish (fish_id)
+);
+
+DROP TABLE IF EXISTS [fish_types]
+
+CREATE TABLE [fish_types] (
+    fish_type VARCHAR(70) PRIMARY KEY 
+);
+
+/* Found data on https://fiskeriforum.no/fiskearter-i-saltvann/ */
+INSERT INTO [fish_types] (fish_types)
+    VALUES 
+    ('Lyr'),
+    ('Torsk'),
+    ('Sei'),
+    ('Hyse'),
+    ('Hvitting'),
+    ('Kolmule'),
+    ('Breiflabb'),
+    ('Lange'),
+    ('Brosme'),
+    ('Gråsteinbit'),
+    ('Rognkjeks'),
+    ('Uer'),
+    ('Berggylt'),
+    ('Blaastaal'),
+    ('Gronngylt'),
+    ('Bergnebb'),
+    ('Rodnebb'),
+    ('Ulke'),
+    ('Tangsprell'),
+    ('Tangsnelle'),
+    ('Tangstikling'),
+    ('Kystringbuk'),
+    ('Trepigget stingsild'),
+    ('Brisling'),
+    ('Lodde'),
+    ('Sild'),
+    ('Makrell'),
+    ('orret'),
+    ('Laks'),
+    ('Rødspette'),
+    ('Sandflyndre'),
+    ('Lomre'),
+    ('Skrubbe'),
+    ('Piggskate'),
+    ('Horngjel'),
+    ('Fjesing'),
+    ('Floyfisk'),
+    ('Paddetorsk'),
+    ('Havabbor'),
+    ('St. Petersfisk'),
+    ('Mulle'),
+    ('Pigghaa'),
+    ('Svarthaa'),
+    ('Haagjel'),
+    ('Håbrann'),
+    ('Slimaal'),
+    ('Laksesild'),
+    ('Lysing'),
+    ('Mora'),
+    ('Havbrasme'),
+    ('Brudefisk'),
+    ('Vassild'),
+    ('Skolest')
+;
