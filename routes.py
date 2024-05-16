51af5e6e1d21868ef316793b2a31d7e75f0a6a06
Wrhyk <Williamkarlsen1@hotmail.com>
2024-05-16 16:16:52 +0200

    new key

--- routes.py
+++ routes.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, render_template, request, redirect, url_for, g
 from setup import add_fish, add_user, select_user
 app = Flask(__name__)
 
-app.secret_key = "lmao"
+app.secret_key = "VERY_SECRET"
 
 DATABASE = './database.db'
 
